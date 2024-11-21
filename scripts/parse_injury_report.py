46805bfa83a5ed30e6821102d34bc4724b264110
tyler-tee <64701075+tyler-tee@users.noreply.github.com>
2024-11-21 08:34:04 -0500

    linting

--- scripts/parse_injury_report.py
+++ scripts/parse_injury_report.py
@@ -2,7 +2,8 @@ import re
 import requests
 from bs4 import BeautifulSoup
 
-def main(input):
+
+def main():
     response = requests.get('https://football.fantasysports.yahoo.com/f1/gamedaycalls')
 
     soup = BeautifulSoup(response.content, 'lxml')
@@ -11,16 +12,16 @@ def main(input):
     headers = [heading.text for heading in table.find_all('th')]
 
     table_rows = [row for row in table.find_all('tr')]
-        
+
     results = [{headers[index]: cell.text for index, cell in enumerate(row.find_all("td"))} for row in table_rows]
     results = [result for result in results if result]
-        
+
     for result in results:
-      for i, v in result.items():
-        for substring in ['\n', ' No new player Notes', ' Player Note', ' New']:
-          v = v.replace(substring, '')
-        result[i] = v
-            
+        for i, v in result.items():
+            for substring in ['\n', ' No new player Notes', ' Player Note', ' New']:
+                v = v.replace(substring, '')
+            result[i] = v
+
         try:
             name_pattern = r'[A-Z][^A-Z]*'
             result['Name'] = ' '.join(re.findall(name_pattern, result['Name'])[:2]).strip()
